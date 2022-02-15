743b62bd089ee4dc6679d87d95953f63400ce2b7
qbhy <96qbhy@gmail.com>
2022-02-15 17:59:04 +0800

    feat: 添加默认参数

---
+++ utils/argument.go
@@ -0,0 +1,102 @@
+package utils
+
+import "github.com/goal-web/contracts"
+
+func DefaultString(values []string, defaultValue ...string) string {
+	if len(values) > 0 {
+		return values[0]
+	}
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return ""
+}
+
+func DefaultInt(values []int, defaultValue ...int) int {
+	if len(values) > 0 {
+		return values[0]
+	}
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return 0
+}
+
+func DefaultInt64(values []int64, defaultValue ...int64) int64 {
+	if len(values) > 0 {
+		return values[0]
+	}
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return 0
+}
+
+func DefaultFloat(values []float32, defaultValue ...float32) float32 {
+	if len(values) > 0 {
+		return values[0]
+	}
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return 0
+}
+
+func DefaultFloat64(values []float64, defaultValue ...float64) float64 {
+	if len(values) > 0 {
+		return values[0]
+	}
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return 0
+}
+
+func DefaultBool(values []bool, defaultValue ...bool) bool {
+	if len(values) > 0 {
+		return values[0]
+	}
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return false
+}
+
+func DefaultInterface(values []interface{}, defaultValue ...interface{}) interface{} {
+	if len(values) > 0 {
+		return values[0]
+	}
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return nil
+}
+
+func DefaultError(values []error, defaultValue ...error) error {
+	if len(values) > 0 {
+		return values[0]
+	}
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return nil
+}
+
+func DefaultFields(values []contracts.Fields, defaultValue ...contracts.Fields) contracts.Fields {
+	if len(values) > 0 {
+		return values[0]
+	}
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
+	}
+
+	return nil
+}
