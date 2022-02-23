087afd3662bbf79a4ffac3ed3440c0186fcc26b0
qbhy <96qbhy@gmail.com>
2022-02-23 11:04:43 +0800

    feat: 添加哈希辅助函数

--- utils/hash.go
+++ utils/hash.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"hash"
 )
 
 // Md5 生成 md5 字符串
@@ -11,4 +12,9 @@ func Md5(v string) string {
 	m := md5.New()
 	m.Write(d)
 	return hex.EncodeToString(m.Sum(nil))
+}
+
+func Hash(hash hash.Hash, data []byte) string {
+	hash.Write(data)
+	return hex.EncodeToString(hash.Sum(nil))
 }
